60c9596f3c103c2ba86b400d2471cccd6e61a76c
Andreas Jung <yet@gmx.de>
2023-08-06 16:34:42 +0200

    updated

--- functions/redirect.js
+++ functions/redirect.js
@@ -1,5 +1,5 @@
 export function onRequest(context) {
     console.log(context);
 /*    return Response.redirect("https://heise.de/?foo=" + context.url);*/
-    return Response("hello");
+    return new Response("hello");
 }
