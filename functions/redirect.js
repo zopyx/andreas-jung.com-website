d2aa0a982d484ce5fa7f3947b9cbb9bd13a16078
Andreas Jung <yet@gmx.de>
2023-08-06 17:18:43 +0200

    updated

--- functions/redirect.js
+++ functions/redirect.js
@@ -1,7 +1,15 @@
-export function onRequest(context) {
-    console.log("a")
-    console.log(context.constructor.name);
-    console.log("b")
-/*    return Response.redirect("https://heise.de/?foo=" + context.url);*/
-    return new Response("hello1");
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  // Log the request URL to the console
+  console.log('Request URL:', request.url)
+
+  // Your logic for handling the request goes here
+  // For example, you can make a fetch request to a backend server and return its response
+  const response = await fetch(request)
+
+  return response
 }
+
